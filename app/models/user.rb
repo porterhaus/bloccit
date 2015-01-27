bf2543d5f5ff7ca7d0fe3808dbd258ac1e1b264a
porterhaus <bporter@porterhaus.com>
2015-01-26 19:19:11 -0600

    Pundit changes that are causing Missing Helper Error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
+  
+  def admin?
+    role=='admin'
+  end
+
+  def moderator?
+    role=='moderator'
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
