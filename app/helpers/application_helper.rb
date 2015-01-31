2ed05d3cac5dd9dcbc10317a60d6259217934e48
porterhaus <bporter@porterhaus.com>
2015-01-31 16:38:09 -0600

    Markdown added as helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,11 +5,18 @@ def my_name
 end
 
 def form_group_tag(errors, &block)
-   if errors.any?
-     content_tag :div, capture(&block), class: 'form-group has-error'
-   else
-     content_tag :div, capture(&block), class: 'form-group'
-   end
+ if errors.any?
+   content_tag :div, capture(&block), class: 'form-group has-error'
+ else
+   content_tag :div, capture(&block), class: 'form-group'
  end
+end
+
+def markdown(text)
+  renderer = Redcarpet::Render::HTML.new
+  extensions = {fenced_code_blocks: true}
+  redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+  (redcarpet.render text).html_safe
+end
 
 end
