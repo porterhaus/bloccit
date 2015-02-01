ac2e6710eac36c7cf590fb3192d23965fc21b176
porterhaus <bporter@porterhaus.com>
2015-02-01 16:53:50 -0600

    Fixing production AWS config.

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'], 
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'], 
   }
-  config.fog_directory  = 'porterhaus-bloccit-development'
+  config.fog_directory  = 'porterhaus-bloccit-production'
   config.fog_public     = true
 end
 
