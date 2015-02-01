255670deca118d614eef77af7b76a70eb1463e24
porterhaus <bporter@porterhaus.com>
2015-02-01 16:34:57 -0600

    Finished adding picture uploading functionality. Bootstrap not working correctly.

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIAILDHPGENK6M4DQYQ',
-    :aws_secret_access_key  => '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'
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'], 
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'], 
   }
   config.fog_directory  = 'porterhaus-bloccit-development'
   config.fog_public     = true
