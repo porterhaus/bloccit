5c69b436605dbe12564c0108e4e01b5bb7941fac
porterhaus <bporter@porterhaus.com>
2015-02-04 17:33:05 -0600

    Made changes to routing an appliction policy.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,13 @@
 class CommentsController < ApplicationController
   
   def new
-    @topic = Topic.find(params[:topic_id])
+    #@topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id])
     @comment = Comment.new
   end
 
   def create
-    @topic = Topic.find(params[:topic_id])
+    #@topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id]) # post_id
     @comment = current_user.comments.build(comment_params)
     @comment.post = @post
@@ -16,25 +16,25 @@ class CommentsController < ApplicationController
 
     if @comment.save
       flash[:notice] = "Comment was saved."
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     else
       flash[:error] = "There was an error saving the comment. Please try again."
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     end
   end
 
   def destroy
-     @topic = Topic.find(params[:topic_id])
-     @post = @topic.posts.find(params[:post_id])
+     #@topic = Topic.find(params[:topic_id])
+     @post = Post.find(params[:post_id])#@topic.posts.find(params[:post_id])
      @comment = @post.comments.find(params[:id])
  
      authorize @comment
      if @comment.destroy
        flash[:notice] = "Comment was removed."
-       redirect_to [@topic, @post]
+       redirect_to [@post.topic, @post]
      else
        flash[:error] = "Comment couldn't be deleted. Try again."
-       redirect_to [@topic, @post]
+       redirect_to [@post.topic, @post]
      end
    end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,21 @@
 Rails.application.routes.draw do
-
   #get 'comments_controller/create'
 
   devise_for :users
   resources :users, only: [:update]
   
+  #resources :topics do
+  #  resources :posts, except: [:index] do
+  #    resources :comments, only: [:create, :destroy]
+  #  end
+  #end
+
   resources :topics do
-    resources :posts, except: [:index] do
-      resources :comments, only: [:create, :destroy]
-    end
+   resources :posts, except: [:index]
+  end
+
+  resources :posts, only: [] do
+   resources :comments, only: [:create, :destroy]
   end
 
   get 'about' => 'welcome#about'
