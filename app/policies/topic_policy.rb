590e179f05a3df0192e3bf879ba24ed6e8fba90d
porterhaus <bporter@porterhaus.com>
2015-02-11 16:19:09 -0600

    Public v. Private Topics

--- app/policies/topic_policy.rb
+++ app/policies/topic_policy.rb
@@ -3,6 +3,10 @@ class TopicPolicy < ApplicationPolicy
    def index?
      true
    end
+
+   def show?
+     record.public? || user.present?
+   end
  
    def create?
      user.present? && user.admin?
