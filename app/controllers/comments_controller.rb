1d042c4cf5be7c25e8d3a01a9fcedd3983d4dc18
porterhaus <bporter@porterhaus.com>
2015-02-14 11:52:51 -0600

    Complete creating with Ajax assignment.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,15 +12,20 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id]) # post_id
     @comment = current_user.comments.build(comment_params)
     @comment.post = @post
+    @new_comment = Comment.new
   
     authorize @comment
 
     if @comment.save
       flash[:notice] = "Comment was saved."
-      redirect_to [@post.topic, @post]
+      #redirect_to [@post.topic, @post]
     else
       flash[:error] = "There was an error saving the comment. Please try again."
-      redirect_to [@post.topic, @post]
+      #redirect_to [@post.topic, @post]
+    end
+
+    respond_with(@comment) do |format|
+      format.html { redirect_to [@post.topic, @post] }
     end
   end
 
@@ -40,6 +45,8 @@ class CommentsController < ApplicationController
        format.html { redirect_to [@post.topic, @post] }
      end
    end
+
+  private
 
   def comment_params
     params.require(:comment).permit(:body)
